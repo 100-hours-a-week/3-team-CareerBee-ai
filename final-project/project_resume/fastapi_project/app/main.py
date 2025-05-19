fc4f35e30921737fac5e39bf9529fc1b28c636e6
ellina-kim <lovekyll0@gmail.com>
2025-05-19 16:31:50 +0900

    Delete final-project/project_resume/fastapi_project/app directory

--- final-project/project_resume/fastapi_project/app/main.py
+++
@@ -1,6 +0,0 @@
-# app/main.py
-from fastapi import FastAPI
-from app.routes import resume_extract
-
-app = FastAPI()
-app.include_router(resume_extract.router, prefix="/resume", tags=["Resume Extract"])
