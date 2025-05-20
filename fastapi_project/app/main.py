8892266509c7d7765108a6478e034e7a53f0eb0a
dain-son <sea000415@gmail.com>
2025-05-20 13:21:22 +0900

    main.py 수정: 피드백 라우터 포함

--- fastapi_project/app/main.py
+++ fastapi_project/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routes import health, resume_create
+from app.routes import health, resume_create, feedback
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,3 +8,4 @@ app = FastAPI()
 
 app.include_router(resume_create.router, tags=["Resume"])
 app.include_router(health.router)
+app.include_router(feedback.router, tags=["Feedback"])
